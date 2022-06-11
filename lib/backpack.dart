bb9efac129f76c19eafe155b81fe5d3465218ac2
mpierog <mycrapx@gmail.com>
2022-06-11 20:48:29 -0300

    debounce

--- lib/backpack.dart
+++ lib/backpack.dart
@@ -734,3 +734,21 @@ class Tex extends StatelessWidget {
     return retWidget;
   }
 }
+
+class Debounce {
+  Duration delay;
+  Timer? _timer;
+
+  Debounce(
+    this.delay,
+  );
+
+  call(void Function() callback) {
+    _timer?.cancel();
+    _timer = Timer(delay, callback);
+  }
+
+  dispose() {
+    _timer?.cancel();
+  }
+}
